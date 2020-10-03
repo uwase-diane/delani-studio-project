a3bdbfee6c90174a904f5bdad7fda6ab8df0a806
uwase-diane <diane.uwase13@gmail.com>
2020-10-03 06:18:34 -0400

    added texts using jquery

---
+++ index.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $(".image-work").click(function(){
+        $(".text-work").show();
+        $(".image-work").hide();
+    });
+  
+    $(".text-work").click(function(){
+        $(".image-work").show();
+        $(".text-work").hide();
+    });
+
+});
+
+$(document).ready(function(){
+
+
+$(".imag-work").click(function(){
+    $(".tet-work").show();
+    $(".imag-work").hide();
+});
+$(".tet-work").click(function(){
+    $(".imag-work").show();
+    $(".tet-work").hide();
+});
+
+});
+
+$(document).ready(function(){
+    $(".img-work").click(function(){
+        $(".txt-work").show();
+        $(".img-work").hide();
+    });
+    $(".txt-work").click(function(){
+        $(".img-work").show();
+        $(".txt-work").hide();
+    });
+    
+});
+
+// jquery overaly images
+$(document).ready(function(){
+    $(".portfolio-img1").hover(function(){
+        $(".overlay1").toggle();
+    });
+});
